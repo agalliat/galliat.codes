53bd04610623d4e3c862755defd4109fb65ccbd9
Angela Galliat <42209272+agalliat@users.noreply.github.com>
2022-04-21 22:05:35 +0200

    Update siteconfig.js

--- content/_data/siteconfig.js
+++ content/_data/siteconfig.js
@@ -26,7 +26,7 @@ module.exports = {
     // YouTube ID/Channel  (optional, remove it not needed), used for link in the left sidebar
     //socialYouTube: "agalliat",
     // Google Analytics ID  (optional, remove it not needed), used for... well, Google Analytics
-    //googleAnalytics: "YOUR_GA-ID",
+    googleAnalytics: "G-YV6KG4B862",
     // Algolia-powered search  (optional, remove it not needed),
     // See: https://github.com/algolia/algoliasearch-netlify
     algoliaSearch: {
