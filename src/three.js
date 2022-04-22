8dbdd90ebc94bc4017ba351d912340ab5a78033d
Lea Rosema <lea@lea.codes>
2022-04-22 14:59:05 +0200

    feat: add second bundle.

---
+++ src/three.js
@@ -0,0 +1,8 @@
+import * as THREE from "three";
+
+console.log(THREE);
+
+const canvas = document.querySelector("#homeCanvas");
+const ctx = canvas.getContext("2d");
+ctx.fillStyle = "red";
+ctx.fillRect(100, 100, 100, 100);
